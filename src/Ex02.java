b49da3f732129e6fb765e27eef3bed0f819c149e
Jean <jeanmatheus0612@gmail.com>
2025-02-25 18:55:18 -0300

    Finalizando o exercicio 2

--- src/Ex02.java
+++ src/Ex02.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class Ex02 {
+    public static void main(String[] args) {
+        Scanner dadoDigitado = new Scanner(System.in);
+
+        System.out.println("Digite o Fahrenheit: ");
+        double fahrenheit = dadoDigitado.nextDouble();
+
+        double celcius = (fahrenheit - 32) * ((double) 5 / 9);
+
+        System.out.println("A conversão de F para C é de " + celcius);
+    }
 }
